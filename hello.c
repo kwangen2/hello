ccd1735080e1870fabde41c36a76a59ce052e2c0
Kaden <kwangen2@huskers.unl.edu>
2024-08-29 10:47:03 -0500

    added my major to the output

---
+++ hello.c
@@ -0,0 +1,17 @@
+/**
+ * Author: Kaden Wangen
+ *         kwangen2@huskers.unl.edu
+ * Date: 2024/08/11
+ *
+ * A simple hello world program in C
+ *
+ */
+#include <stdlib.h>
+#include <stdio.h>
+
+int main(int argc, char **argv) {
+
+  printf("Kaden Wangen!\n");
+  printf("My major is Computer Science!\n");
+  return 0;
+}
